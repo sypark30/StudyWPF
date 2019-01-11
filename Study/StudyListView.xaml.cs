10295a99b6c9e055a7756ff7f3aac0abb5a386ee
sypark <prizero127@gmail.com>
2019-01-11 23:52:07 +0900

    home

---
+++ Study/StudyListView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudyWPF
+{
+    /// <summary>
+    /// ListView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ListView : Window
+    {
+        public ListView()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGetData_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
